2816ed83498f8992c54829500f609b08cf8bac7d
Jeremy Morrison <36835728+JeMorriso@users.noreply.github.com>
2023-03-11 10:58:45 -0800

    add my plugins and config

---
+++ lua/plugins/plugins.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "camgraff/telescope-tmux.nvim",
+    dependencies = { "norcalli/nvim-terminal.lua" },
+  },
+  {
+    "tidalcycles/vim-tidal",
+  },
+  {
+    "jpalardy/vim-slime",
+  },
+}
