da2f09289b559f6e471e9d1ef36cd718f4dce6f9
Jeremy Morrison <36835728+JeMorriso@users.noreply.github.com>
2023-03-11 22:32:44 -0800

    add a few keymaps and options

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set("", "<localleader>y", '"+y', { noremap = true, silent = true })
+vim.keymap.set("", "<localleader>p", '"+p', { noremap = true, silent = true })
+
+vim.keymap.set("n", "<localleader>d", [[:put =strftime('%A, %B %d, %Y')<cr>kJ]], { noremap = true, silent = true })
+vim.keymap.set("n", "<localleader>t", [[:put =strftime('%T')<cr>kJ]], { noremap = true, silent = true })
