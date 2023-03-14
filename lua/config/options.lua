598c10ba210cc1ab9a1073d17063abd4c2584fde
Jeremy Morrison <36835728+JeMorriso@users.noreply.github.com>
2023-03-13 22:22:43 -0700

    add tidal config / keybinding

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -11,5 +11,6 @@ vim.g.maplocalleader = [[;]]
 
 vim.g.slime_target = "tmux"
 
+vim.g.tidal_no_mappings = 1
 vim.g.tidal_target = "tmux"
 vim.g.tidal_default_config = { socket_name = "default", target_pane = ":.1" }
